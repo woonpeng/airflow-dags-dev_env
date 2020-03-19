f80209761a26e9882d4f2a1b66dec5881ad71772
wp <deadstroke@gmail.com>
2020-03-19 11:24:51 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,14 @@
 docker start hadoop-master-dd
 docker start hadoop-slave1-dd
 docker start hadoop-slave2-dd
-docker start hive-db-dd
+#docker start hive-db-dd
 docker start mysql-hive-dd
+docker start neo4j
 
 docker exec -it hadoop-master-dd bash -c "service sshd restart"
 docker exec -it mysql-hive-dd bash -c "service mysqld restart"
 
-docker exec -d hive-db-dd hive --service metastore
+#docker exec -d hive-db-dd hive --service metastore
 docker exec -it --user hadoop hadoop-master-dd bash -c "/usr/local/hadoop/sbin/start-all.sh"
 docker exec --user hadoop hadoop-master-dd hadoop/bin/hdfs dfs -mkdir /user
 docker exec --user hadoop hadoop-master-dd hadoop/bin/hdfs dfs -mkdir /user/hive
