ebaef2b4b6f6d1dfec430ee523f28b722be2e74a
wp <deadstroke@gmail.com>
2020-06-30 10:12:15 +0800

    dev env update

--- copy_keys.sh
+++ copy_keys.sh
@@ -1,3 +1,2 @@
-mkdir -p ~/hmaster-dd_keys
-docker cp hadoop-master-dd:/home/hadoop/.ssh/id_rsa ~/hmaster-dd_keys/id_rsa
-docker cp hadoop-master-dd:/home/hadoop/.ssh/id_rsa.pub ~/hmaster-dd_keys/id_rsa.pub
+docker cp hadoop-master-dd:/home/hadoop/.ssh/id_rsa ./secrets/id_rsa
+docker cp hadoop-master-dd:/home/hadoop/.ssh/id_rsa.pub ./secrets/id_rsa.pub
