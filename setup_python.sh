aade0cf1650e61008315ab019a916dfb21519597
wp <deadstroke@gmail.com>
2020-12-16 12:09:41 +0800

    update pip

--- setup_python.sh
+++ setup_python.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # create Python 3 environment
+docker exec -it hadoop-master-dd python3 -m pip install --upgrade pip
 docker exec -it hadoop-master-dd python3 -m pip install -r airflow-dags/requirements.txt
 docker exec -it --user hadoop hadoop-master-dd python3 -m ipykernel install --user --name airflow-dags_py3 --display-name "airflow-dags (Python 3)"
 
